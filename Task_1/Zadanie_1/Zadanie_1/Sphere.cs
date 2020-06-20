6f7b8a9dd59949d2297dbe31f127562fac166687
Jakub <kuba.urbanski22@gmail.com>
2020-06-20 14:17:43 +0200

    Vector class settters and constructor added; Sphere constructor and countDiscriminant added

--- Task_1/Zadanie_1/Zadanie_1/Sphere.cs
+++ Task_1/Zadanie_1/Zadanie_1/Sphere.cs
@@ -6,5 +6,21 @@ namespace Zadanie_1
 {
     class Sphere
     {
+        private int x, y, z, r;
+        private Vector vector;
+        public Sphere(int x, int y, int z, int r)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.r = r;
+        }
+
+        //TODO: implement function to know if there was crossing point/s of sphere and line
+        public int countDiscriminant()
+        {
+            //TODO: placeholer, to be changed to true value
+            return 0;
+        }
     }
 }
--- Task_1/Zadanie_1/Zadanie_1/Vector.cs
+++ Task_1/Zadanie_1/Zadanie_1/Vector.cs
@@ -4,9 +4,28 @@ namespace Zadanie_1
 {
     class Vector
     {
-        static void Main(string[] args)
+        private int x, y, z;
+
+        public Vector(int x, int y, int z)
         {
-            Console.WriteLine("Hello World!");
+            setX(x);
+            setY(y);
+            setZ(z);
+        }
+
+        private void setX(int _x)
+        {
+            this.x = _x;
+        }
+
+        private void setY(int _y)
+        {
+            this.y = _y;
+        }
+
+        private void setZ(int _z)
+        {
+            this.z = _z;
         }
     }
 }
